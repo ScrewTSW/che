feec549c69627121f63e5fe305817c12c5a33a64
Tibor Dancs <tdancs@redhat.com>
2020-01-14 12:27:07 +0100

    Prepaired base objects for NotificationContainer and Notification

---
+++ tests/e2e/pageobjects/ide/NotificationContainer.ts
@@ -0,0 +1,22 @@
+/*********************************************************************
+ * Copyright (c) 2019 Red Hat, Inc.
+ *
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ **********************************************************************/
+
+import { injectable, inject } from 'inversify';
+import { CLASSES } from '../../inversify.types';
+import { DriverHelper } from '../../utils/DriverHelper';
+import { By, Key, WebElement, error } from 'selenium-webdriver';
+import { TestConstants } from '../../TestConstants';
+import { Logger } from '../../utils/Logger';
+
+@injectable()
+export class NotificationContainer {
+    constructor(@inject(CLASSES.DriverHelper) private readonly driverHelper: DriverHelper) { }
+
+}
